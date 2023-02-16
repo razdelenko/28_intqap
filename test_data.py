35f2ee5f058bdd17a12bbaecf4cd94599ac5e766
Roma <116964145+razdelenko@users.noreply.github.com>
2023-02-16 19:52:36 +0500

    Update test_data.py

--- test_data.py
+++ test_data.py
@@ -4,20 +4,20 @@ class Invalid_Data:
     fake_email = Faker().email()
     fake_password = Faker().password()
     fake_name = Faker().name()
-    first_name_1_char = 'М'
-    first_name_31_char = 'МуркаМуркаМуркаМуркаМуркаМуркаМ'
-    last_name_1_char = 'У
-    last_name_31_char = 'МуровнаМуровнаМуровнаМуровнаМур'
-    password_21_char = 'asd123dsaasd123dsaasd'
+    first_name_1_char = 'Р'
+    first_name_31_char = 'РоманРоманРоманРоманРоманРоманР'
+    last_name_1_char = 'К'
+    last_name_31_char = 'КольцовКольцовКольцовКольцовКол'
+    password_21_char = '123qwertyqwertyqwerty'
     password_no_Lower = 'qwerty'
     password_9_char = 'qwerty123
-    password_not_contain_digit = "Qwertyui"
-    xss = '<script>qwerty(123)</script>'
-    email_without_domain = 'asd123@mail.ru
+    password_not_contain_digit = 'Qwertyuiop'
+    xss = '<script>alert(123)</script>'
+    email_without_domain = 'abc123@mail'
     invalid_phoneNumber = '+71111111111'
 
 class Valid_Data:
-    valid_first_name = 'Диана'
-    valid_last_name = 'Бабурина'
-    valid_password = 'qwerty'
-    valid_phoneNumber = '+7999999555'
+    valid_first_name = 'Роман'
+    valid_last_name = 'Кольцов'
+    valid_password = 'Qwerty36'
+    valid_phoneNumber = '+77081095251'
